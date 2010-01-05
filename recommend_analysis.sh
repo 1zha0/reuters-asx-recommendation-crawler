5ece4b3db5fd83b5918c5add88f2f42ae7159f17
lzhao <lzhao@cse.unsw.edu.au>
2010-01-05 13:47:43 +0000

    Change wget address and awk paramters since Reuters has changed its website.

--- recommend_analysis.sh
+++ recommend_analysis.sh
@@ -3,6 +3,9 @@
 # This script pulls newest company list from ASX, and then, retrieves
 # recommendation index from Reuters.
 #
+# Update: Jan 6, 2010, Change wget address and awk command, since 
+#                      Reuters has changed its website.
+#
 # Written by:	Liang
 # Date:		25/11/2009
 #
@@ -16,5 +19,5 @@ wget -N http://www.asx.com.au/asx/research/$CO_LIST
 echo "ASX Code, Recommendation" > $RESULT
 for i in `awk -F'",|,"' '{print $2}' $CO_LIST`
 do
-	echo "$i, `wget http://www.reuters.com/finance/stocks/recommendations?symbol=$i.AX -O - | grep '<div title="[0-9.]*">' | awk -F'"' '{print $2}'`" >> $RESULT
+	echo "$i, `wget http://www.reuters.com/finance/stocks/overview?symbol=$i.AX -O - | grep 'alt="Analyst Recommendations" title="[0-9.]*" />' | awk -F'"' '{print $10}'`" >> $RESULT
 done
